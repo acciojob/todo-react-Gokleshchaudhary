1852e6c7fbb1d6d13c3767f649789bd3de33b9ee
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 13:13:50 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,1 +1,19 @@
+import React from 'react';
+
+function Todo({ todos, onDelete }) {
+  return (
+    <ul className="todo-list">
+      {todos.map((todo, index) => (
+        <li key={index} className="todo-item">
+          <span>{todo}</span>
+          <button className="delete-btn" onClick={() => onDelete(index)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Todo;
 
